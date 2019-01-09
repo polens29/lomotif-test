28810d9ee2788a15a7e3879fb0ac960082a286a6
Pauline Añasco <polens29@gmail.com>
2019-01-09 16:19:56 +0800

    add comments and requirements.txt

--- backend/webapp/views.py
+++ backend/webapp/views.py
@@ -1,29 +1,36 @@
 import random
 
 from django.db.models import Max, Q
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from webapp.serializers import CardSerializer
 from .models import Card
 
-# Create your views here.
+
 class CardView(APIView):
 
     def get(self, request, playerClass):
         playerClass = playerClass.title()
+
+        # Get max ID from Card model
         max_id = Card.objects.all().aggregate(max_id=Max("id"))['max_id']
         cards = []
+
+        # Get 30 cards
         while len(cards) < 30:
+            # Pick random number from 1 to max ID
             pk = random.randint(1, max_id)
-            try:
-                card = Card.objects.filter(Q(playerClass=playerClass) | Q(playerClass='Neutral'), pk=pk).exclude(name="").first()
-                if card:
-                    serialized_card = CardSerializer(card).data
-                    if cards.count(serialized_card) < 2:
-                        cards.append(serialized_card)
-            except:
-                pass
 
+            # Filter card with random number and playerClass
+            card = Card.objects.filter(Q(playerClass=playerClass) | Q(playerClass='Neutral'), pk=pk).first()
+            if card:
+                serialized_card = CardSerializer(card).data
 
-        return Response(cards)
+                # Maximum of two same cards
+                if cards.count(serialized_card) < 2:
+                    cards.append(serialized_card)
+
+        return Response(cards, status=status.HTTP_200_OK)
+    
